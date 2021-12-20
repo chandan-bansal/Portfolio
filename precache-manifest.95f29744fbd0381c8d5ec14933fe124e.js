8ff588e60b0fb14dfe1c217015f58e83d558c7bc
chandan-bansal <57485353+chandan-bansal@users.noreply.github.com>
2021-12-20 14:11:48 +0530

    Updates

---
+++ precache-manifest.95f29744fbd0381c8d5ec14933fe124e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c3f6557e6985aa3aa3a0519e5dcb27c",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "08ebef4a5bc05adee162",
+    "url": "/Portfolio/static/css/main.d0acee29.chunk.css"
+  },
+  {
+    "revision": "d73ea675c45b43a980ec",
+    "url": "/Portfolio/static/js/2.5c20e2d1.chunk.js"
+  },
+  {
+    "revision": "3143274d7d26932c98894a52e75c487a",
+    "url": "/Portfolio/static/js/2.5c20e2d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "08ebef4a5bc05adee162",
+    "url": "/Portfolio/static/js/main.4393999c.chunk.js"
+  },
+  {
+    "revision": "9dd11279386edbd5cc4f",
+    "url": "/Portfolio/static/js/runtime-main.9b4bb9e8.js"
+  },
+  {
+    "revision": "70db0fcc40256b3ce85f2b203f9f096b",
+    "url": "/Portfolio/static/media/Chandan_img.70db0fcc.jpg"
+  }
+]);
